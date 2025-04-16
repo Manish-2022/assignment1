38c064304535709e12e4214150035394f91580d2
Manish-2022 <manish.mohapatra@tigeranalytics.com>
2025-04-16 15:49:39 +0530

    code to get file names

--- script.py
+++ script.py
@@ -2,15 +2,37 @@ import os
 
 from azure.storage.blob import BlobServiceClient
 from dotenv import load_dotenv
+from flask import Flask, jsonify, request
 
 load_dotenv("dev.env")
 
 CONTAINER_NAME = os.getenv("CONTAINER_NAME")
 
-connection_string = os.getenv("CONNECTION_STRING")
-blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-container_client = blob_service_client.get_container_client(CONTAINER_NAME)
+app = Flask(__name__)
 
-# List blobs
-for blob in container_client.list_blobs():
-    print(blob.name)
+
+@app.route("/get-data", methods=["GET"])
+def get_data():
+    prefix = request.args.get("prefix")
+    connection_string = os.getenv("CONNECTION_STRING")
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    container_client = blob_service_client.get_container_client(CONTAINER_NAME)
+    blobs = container_client.list_blob_names(name_starts_with=prefix)
+    # List blobs
+    response = []
+    for blob in blobs:
+        response.append(blob.replace(f"{prefix}/", ""))
+    if len(response) == 0:
+        return (
+            jsonify(
+                {
+                    "error": f"Either there is no folder named {prefix} or there are no files inside the folder"
+                }
+            ),
+            400,
+        )
+    return jsonify(response), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
