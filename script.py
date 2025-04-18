8820677f55de726db6dc89767a6ccb1c52a7a298
Manish-2022 <manish.mohapatra@tigeranalytics.com>
2025-04-18 12:39:10 +0530

    added gunicorn

--- script.py
+++ script.py
@@ -37,5 +37,5 @@ def get_data():
     return jsonify(response), 200
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
